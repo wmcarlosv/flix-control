535cf48d1b3d4d3c303a5f8d4fa4a9ec23b7e616
Carlos Vargas <desarrolloarve@gmail.com>
2024-01-14 16:23:34 -0400

    mejorando

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -40,7 +40,7 @@
 
     <div class="row">
         <div class="col-md-12">
-            <div class="card card-info">
+            <div class="card">
             <div class="card-header">
                 <h3>Ultimos Movimientos</h3>
             </div>
